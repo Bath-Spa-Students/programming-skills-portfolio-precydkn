e6ed9e11f12717131f79ea9f161c1350abbcc4c8
precydkn <144213767+precydkn@users.noreply.github.com>
2023-10-12 10:35:34 +0300

    Create exercise2.py

---
+++ Chapter 2- Variables & Comments/Exercises/exercise2.py
@@ -0,0 +1,6 @@
+'''Exercise 2: Find a quote from a famous person you admire. Print the quote and the name of its author. 
+Your output should look something like the following, including the quotation marks:
+Albert Einstein once said, “A person who never made a mistake never tried anything new.'''
+
+quote="It does not matter how slowly you go as long as you do not stop."
+print(f'Confucius once said, "{quote}"')
