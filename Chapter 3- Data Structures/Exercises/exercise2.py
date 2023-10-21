1b9640655f3313747cbeefeb423120fe2a6dfb84
precydkn <144213767+precydkn@users.noreply.github.com>
2023-10-21 14:04:26 +0300

    Create exercise2.py

---
+++ Chapter 3- Data Structures/Exercises/exercise2.py
@@ -0,0 +1,10 @@
+"""Exercise 2: Greetings 
+Start with the list you used in Exercise 1, but instead of just printing each person’s name, print a message to them. 
+The text of each message should be the same, but each message should be personalized with the person’s name."""
+
+names=["Angelica", "Rennzo", "Eric", "Temilade", "Seb"]
+print(f'One of the reasons I am still here today is you, {names[0]}.')
+print(f'One of the reasons I am still here today is you, {names[1]}.')
+print(f'One of the reasons I am still here today is you, {names[-3]}.')
+print(f'One of the reasons I am still here today is you, {names[3]}.')
+print(f'One of the reasons I am still here today is you, {names[-1]}.')
