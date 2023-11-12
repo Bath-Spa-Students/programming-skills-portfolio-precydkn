c0ed411e7b2cfb319c40b61d7d41ff10dab50c62
precydkn <144213767+precydkn@users.noreply.github.com>
2023-11-12 10:34:35 +0300

    exercise1.py

---
+++ Chapter 7- Functions/Exercises/exercise1.py
@@ -0,0 +1,11 @@
+## Exercise 1:   Message
+'''Write a function called display_message() that prints one sentence telling everyone what you are learning about in this chapter. Call the
+function, and make sure the message displays correctly.'''
+
+# a function printing a message about what we are learning in this chapter
+def display_message():
+    sentence = "We are learning about functions in this chapter."
+    print(sentence)
+
+# calling the function to show the message
+display_message()
