afb3ce783c73aab55ab75217817d05b71743f3b3
precydkn <144213767+precydkn@users.noreply.github.com>
2023-11-13 10:18:50 +0300

    exercise5.py

--- Chapter 4- Control Flow/Exercises/exercise5.py
+++ Chapter 4- Control Flow/Exercises/exercise5.py
@@ -4,19 +4,21 @@
 •   Write five if statements. Each should check whether a certain kind of fruit is in your list. If the fruit is in your list, 
     the if block should print a statement, such as You really like bananas!'''
 
+# a list of fruit names
 favorite_fruits = ["orange" , "mango" , "grapes"]
 
+# if statements to check whether a fruit name is in the list
 if "orange" in favorite_fruits: 
-    print("You really like oranges!")
+    print("You really like oranges!") # displays a message about the orange because it's part of the list
 
 if "banana" in favorite_fruits:
-    print("You really like bananas!")
+    print("You really like bananas!") # displays nothing because banana is not part of the list
 
 if "grapes" in favorite_fruits: 
-    print("You really like grapes!")
+    print("You really like grapes!") # displays a message about the grapes because it's part of the list
 
 if "mango" in favorite_fruits: 
-    print("You really like mangoes!")
+    print("You really like mangoes!") # displays a message about the mangoes because it's part of the list
 
 if "strawberry" in favorite_fruits:
-    print("You really like strawberries!")
+    print("You really like strawberries!") # displays nothing because strawberry is not part of the list
