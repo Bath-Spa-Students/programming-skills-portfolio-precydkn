530b979f3c0384466066b4423d617b9b27910353
precydkn <144213767+precydkn@users.noreply.github.com>
2023-11-13 09:44:11 +0300

    exercise2.py

--- Chapter 2- Variables & Comments/Exercises/exercise2.py
+++ Chapter 2- Variables & Comments/Exercises/exercise2.py
@@ -2,5 +2,6 @@
 Your output should look something like the following, including the quotation marks:
 Albert Einstein once said, “A person who never made a mistake never tried anything new.'''
 
+# storing a string of quote in a variable
 quote="It does not matter how slowly you go as long as you do not stop."
-print(f'Confucius once said, "{quote}"')
+print(f'Confucius once said, "{quote}"') # displays the quote using f-string
