8a8fdd2a091790c3f6757affbd9f24d9e0d23be5
precydkn <144213767+precydkn@users.noreply.github.com>
2023-11-13 10:09:51 +0300

    exercise1.py

--- Chapter 4- Control Flow/Exercises/exercise1.py
+++ Chapter 4- Control Flow/Exercises/exercise1.py
@@ -4,14 +4,16 @@ Create a variable called alien_color and assign it a value of 'green', 'yellow',
 •   Write an if statement to test whether the alien’s color is green. If it is, print a message that the player just earned 5 points.
 •   Write one version of this program that passes the if test and another that fails. (The version that fails will have no output.)'''
 
-# a passing program for when the alien is green
-alien_color = "green" or "yellow" or "red"
+## a passing program for when the alien is green
+alien_color = "green" or "yellow" or "red" # assigning the alien colors
 
+# if statement about the points earned if the alien color is green
 if alien_color == "green":
-    print("You have just earned 5 points!")
+    print("You have just earned 5 points!") # displays the earned points if the alian color is green
 
-# a failed program for when the alien is not green
-alien_color = "yellow" or "red"
+## a failed program for when the alien is not green
+alien_color = "yellow" or "red" # assigning the alien colors
 
+# if statement about the points earned if the alien is green
 if alien_color == "green":
-    print("You have just earned 5 points!")
+    print("You have just earned 5 points!") # displays nothing because the alien is not green
