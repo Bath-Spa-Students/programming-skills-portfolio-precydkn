9451b7f5d668361b7987ee16daa6074cdaedb8cb
precydkn <144213767+precydkn@users.noreply.github.com>
2023-11-13 09:55:46 +0300

    exercise2.py

--- Chapter 3- Data Structures/Exercises/exercise2.py
+++ Chapter 3- Data Structures/Exercises/exercise2.py
@@ -2,7 +2,9 @@
 Start with the list you used in Exercise 1, but instead of just printing each person’s name, print a message to them. 
 The text of each message should be the same, but each message should be personalized with the person’s name."""
 
+## same list from the previous exercise
 names=["Angelica", "Rennzo", "Eric", "Temilade", "Seb"]
+# displaying the same message but with different names using f-string and list slicing
 print(f'One of the reasons I am still here today is you, {names[0]}.')
 print(f'One of the reasons I am still here today is you, {names[1]}.')
 print(f'One of the reasons I am still here today is you, {names[-3]}.')
