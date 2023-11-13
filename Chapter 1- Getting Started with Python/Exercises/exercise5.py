16a33aebaef7997b5744823e328335eb3efcd3b9
precydkn <144213767+precydkn@users.noreply.github.com>
2023-11-13 09:40:21 +0300

    exercise5.py

--- Chapter 1- Getting Started with Python/Exercises/exercise5.py
+++ Chapter 1- Getting Started with Python/Exercises/exercise5.py
@@ -1,4 +1,5 @@
-from math import pi
-radius = float(input("Input the radius of the circle: "))
-area = pi * (radius**2)
-print("The area of the circle is: %.2f " % area)
+# calculating the area of a circle with user input for the values of the circle
+from math import pi # getting data of the value of pi
+radius = float(input("Input the radius of the circle: ")) # user input for the radius of the circle
+area = pi * (radius**2) # formula for computing the area of circle
+print("The area of the circle is: %.2f " % area) # displays the answer
