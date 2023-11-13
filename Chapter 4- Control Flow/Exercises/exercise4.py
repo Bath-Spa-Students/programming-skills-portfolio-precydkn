7cb19861d65aac937c4b12ee9d7bc7e77975189e
precydkn <144213767+precydkn@users.noreply.github.com>
2023-11-13 10:15:52 +0300

    exercise4.py

--- Chapter 4- Control Flow/Exercises/exercise4.py
+++ Chapter 4- Control Flow/Exercises/exercise4.py
@@ -7,19 +7,21 @@
 •   If the person is at least 20 years old but less than 65, print a message that the person is an adult.
 •   If the person is age 65 or older, print a message that the person is an elder.'''
 
+# assigning an age
 age = 44
 
-print(f"The age of the person is {age}") # show the assigned age
+print(f"The age of the person is {age}") # shows the assigned age
 
+#if-elif-else statement to know what stage in life the person is based on the assigned age
 if age < 2:
-    print("The person is a baby.")
+    print("The person is a baby.") # displays the baby stage
 elif age < 4:
-    print("The person is a toddler.")
+    print("The person is a toddler.") # displays the toddler stage
 elif age < 13:
-    print("The person is a kid.")
+    print("The person is a kid.") # displays the kid stage
 elif age < 20:
-    print("The person is a teenager.")
+    print("The person is a teenager.") # displays the teenager stage
 elif age < 65:
-    print("The person is a adult.")
+    print("The person is a adult.") # displays the adult stage
 else:
-    print("The person is a elder.")
+    print("The person is a elder.") # displays the elder stage
