2707224a7e10281cb789de4d1d57b2579fea6b5e
precydkn <144213767+precydkn@users.noreply.github.com>
2023-11-13 09:58:38 +0300

    exercise3.py

--- Chapter 3- Data Structures/Exercises/exercise3.py
+++ Chapter 3- Data Structures/Exercises/exercise3.py
@@ -2,11 +2,14 @@
 Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. 
 Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.” """
 
+# storing a list of vehicles
 myTranspo=["Volkswagen camper van", "bicycle", "car", "motorcycle"]
+
+# displaying a multi-line string of statements abou the vehicles in the list above. 
 print(f'''I've always liked the look of {myTranspo[0]}s, so I would like to own one of those.
       
 It's been so long since I've ridden a {myTranspo[1]}, but I would like to own a black or blue one and ride it during the winter.
 
 I don't know what brand of {myTranspo[2]}, but I'd like to own one and drive my family and friends wherever they want.
 
-I don't think I'll ever learn to ride one, but I think {myTranspo[3]}s are cool.''')
+I don't think I'll ever learn to ride one, but I think {myTranspo[3]}s are cool.''') # each statement is assigned to one vehicle which was called using f-string.
