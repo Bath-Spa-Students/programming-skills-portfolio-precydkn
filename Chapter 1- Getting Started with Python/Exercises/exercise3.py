2c0240065d2479b796c8256f174ec603dc8700dc
precydkn <144213767+precydkn@users.noreply.github.com>
2023-11-13 09:36:17 +0300

    exercise3.py

--- Chapter 1- Getting Started with Python/Exercises/exercise3.py
+++ Chapter 1- Getting Started with Python/Exercises/exercise3.py
@@ -1,3 +1,4 @@
+# getting system data for teh date and time by using import
 import datetime
-currentTime = datetime.datetime.now()
-print ("Current date and time: ", currentTime.strftime("%m/%d/%Y %H:%M:%S"))
+currentTime = datetime.datetime.now() # storing the current date and time in a variable
+print ("Current date and time: ", currentTime.strftime("%m/%d/%Y %H:%M:%S")) # displaying the current date and time
