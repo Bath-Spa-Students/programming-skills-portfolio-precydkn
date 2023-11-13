24badbe9bd65d2917ecaf9f9c8a008560ae811ed
precydkn <144213767+precydkn@users.noreply.github.com>
2023-11-13 10:11:16 +0300

    exercise2.py

--- Chapter 4- Control Flow/Exercises/exercise2.py
+++ Chapter 4- Control Flow/Exercises/exercise2.py
@@ -4,9 +4,11 @@
 •   If the alien’s color isn’t green, print a statement that the player just earned 10 points.
 •   Write one version of this program that runs the if block and another that runs the else block."""
 
+# assigning a color to the alien
 alien_color = "yellow"
 
+# if-else statement about the points earned based on the color of the alien
 if alien_color == "green":
-    print("Green Alien shot. You earn 5 points!")
+    print("Green Alien shot. You earn 5 points!") # displays 5 points earned if the alien is green
 else:
-    print("Yellow Alien shot. You earn 10 points!")
+    print("Yellow Alien shot. You earn 10 points!") # displays 10 points earned if the alien is yellow
