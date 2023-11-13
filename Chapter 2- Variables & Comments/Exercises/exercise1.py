2bffec51d560010fe7234e12e16baed6edf52150
precydkn <144213767+precydkn@users.noreply.github.com>
2023-11-13 09:43:07 +0300

    exercise1.py

--- Chapter 2- Variables & Comments/Exercises/exercise1.py
+++ Chapter 2- Variables & Comments/Exercises/exercise1.py
@@ -1,8 +1,10 @@
 ''' Exercise 1: Assign a message to a variable, and print that message.
 Then change the value of the variable to a new message, and print the new message.'''
 
+# assigning a value to the variable 'message'
 message="This is the initally assigned message."
-print(message)
+print(message) # displays the message stored
 
-message="This the reassigned message."
-print(message)
+# reassigning a new value to the same variable above
+message="This the reassigned message." 
+print(message) # displays the reassigned value of the same variable
