629c62894eb8849a894c9f2c560339a9f04f4fac
precydkn <144213767+precydkn@users.noreply.github.com>
2023-11-13 09:50:57 +0300

    exercise5.py

--- Chapter 2- Variables & Comments/Exercises/exercise5.py
+++ Chapter 2- Variables & Comments/Exercises/exercise5.py
@@ -2,10 +2,12 @@
 They are £6 each. Write a programme that calculates how many USB sticks she can buy and how many pounds she will have left.
 You will to use the arithmetic operators to complete this exercise.'''
 
+# assigning the values from the problem to variables
 budget = 50
 usbPrice = 6
-numOfUsb = int(budget/usbPrice)
-totalPrice = usbPrice*numOfUsb
-moneyLeft = budget-totalPrice
+numOfUsb = int(budget/usbPrice) # formula to know how many usbs can be bought with the money
+totalPrice = usbPrice*numOfUsb # formula to know the total price of the usbs
+moneyLeft = budget-totalPrice # formula to know the change left for the girl after buying
 
+# displaying the answers to the problem
 print(f'''The number of USB sticks that the girl can buy for £50 is {numOfUsb}.\nThe total price of the {numOfUsb} USB sticks is {totalPrice}, leaving her with £{moneyLeft}.''')
