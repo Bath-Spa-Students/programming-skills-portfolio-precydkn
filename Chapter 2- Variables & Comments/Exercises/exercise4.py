349c46c8ee69575a32fa513fee994e24d49d1370
precydkn <144213767+precydkn@users.noreply.github.com>
2023-11-13 09:48:11 +0300

    exercise4.py

--- Chapter 2- Variables & Comments/Exercises/exercise4.py
+++ Chapter 2- Variables & Comments/Exercises/exercise4.py
@@ -1,5 +1,6 @@
 '''Use a variable to represent your favorite number. Then, using that variable, 
 create a message that reveals your favorite number. Print that message.'''
 
+# storing my favorite number in a variable
 myFavNumber=3
-print(f"My favorite number is {myFavNumber}.")
+print(f"My favorite number is {myFavNumber}.") # without directly writing the number, displays my favorite number using f-string
