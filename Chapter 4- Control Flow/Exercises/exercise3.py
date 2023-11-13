7986cb28065a8a0b34094625d453902d8184b8aa
precydkn <144213767+precydkn@users.noreply.github.com>
2023-11-13 10:13:47 +0300

    exercise3.py

--- Chapter 4- Control Flow/Exercises/exercise3.py
+++ Chapter 4- Control Flow/Exercises/exercise3.py
@@ -5,32 +5,35 @@
 •	 If the alien is red, print a message that the player earned 15 points.
 •	 Write three versions of this program, making sure each message is printed for the appropriate color alien.'''
 
-# program for green alien
+## program for green alien
 alien_color = "green"
 
+#if-elif-else statement about the points earned based on the alien color
 if alien_color == "green":
-    print("Green Alien shot. You earn 5 points!")
+    print("Green Alien shot. You earn 5 points!") # displays 5 points earned if the alien is green
 elif alien_color == "yellow":
-    print("Yellow Alien shot. You earn 10 points!")
+    print("Yellow Alien shot. You earn 10 points!") # displays 10 points earned if the alien is yellow
 else:
-    print("Red Alien shot. You earn 15 points!")
+    print("Red Alien shot. You earn 15 points!") # displays 15 points earned if the alien is a different color
 
-# program for yellow alien
+## program for yellow alien
 alien_color = "yellow"
 
+#if-elif-else statement about the points earned based on the alien color
 if alien_color == "green":
-    print("Green Alien shot. You earn 5 points!")
+    print("Green Alien shot. You earn 5 points!") # displays 5 points earned if the alien is green
 elif alien_color == "yellow":
-    print("Yellow Alien shot. You earn 10 points!")
+    print("Yellow Alien shot. You earn 10 points!") # displays 10 points earned if the alien is yellow
 else:
-    print("Red Alien shot. You earn 15 points!")
+    print("Red Alien shot. You earn 15 points!") # displays 15 points earned if the alien is a different color
 
-# program for red alien
+## program for red alien
 alien_color = "red"
 
+#if-elif-else statement about the points earned based on the alien color
 if alien_color == "green":
-    print("Green Alien shot. You earn 5 points!")
+    print("Green Alien shot. You earn 5 points!") # displays 5 points earned if the alien is green
 elif alien_color == "yellow":
-    print("Yellow Alien shot. You earn 10 points!")
+    print("Yellow Alien shot. You earn 10 points!") # displays 10 points earned if the alien is yellow
 else:
-    print("Red Alien shot. You earn 15 points!")
+    print("Red Alien shot. You earn 15 points!") # displays 15 points earned if the alien is a different color
