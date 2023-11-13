7f737553f1f7a2fd4f384ad19b9240b9a292325b
precydkn <144213767+precydkn@users.noreply.github.com>
2023-11-13 09:38:06 +0300

    exercise4.py

--- Chapter 1- Getting Started with Python/Exercises/exercise4.py
+++ Chapter 1- Getting Started with Python/Exercises/exercise4.py
@@ -1,4 +1,5 @@
+# three values stored in three variables to be concatenated
 course = "Creative Computing "
 yearLevel = "Year 1 "
 group = "Group 1"
-print (course + yearLevel + group)
+print (course + yearLevel + group) # displaying together the values above through string concatenation
