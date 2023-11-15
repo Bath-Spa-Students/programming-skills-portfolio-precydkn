97c260fc792073474216d68d27a56ff231e791cd
precydkn <144213767+precydkn@users.noreply.github.com>
2023-11-15 12:34:14 +0300

    practice5.py

--- Chapter 1- Getting Started with Python/Exercises/practice5.py
+++ Chapter 1- Getting Started with Python/Exercises/practice5.py
@@ -1,6 +1,7 @@
 # Q5: Write four strings in different variables and print the output as one string (String Concatenation).
+# assigning values to variables
 name='Precyleigh '
 verb1='is '
 verb2='programming '
 time='now.'
-print(name + verb1 + verb2 + time)
+print(name + verb1 + verb2 + time) # displays together the assigned values above through string concatenation to form a sentence
