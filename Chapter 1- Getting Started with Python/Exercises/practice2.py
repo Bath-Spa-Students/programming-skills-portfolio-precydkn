af1b889175b2da4248f9e82b67a135755d832174
precydkn <144213767+precydkn@users.noreply.github.com>
2023-11-15 12:33:52 +0300

    practice2.py

--- Chapter 1- Getting Started with Python/Exercises/practice2.py
+++ Chapter 1- Getting Started with Python/Exercises/practice2.py
@@ -1,2 +1,2 @@
 # Q2: Create a program to output "hi" 50 times, without any separator.
-print("hi " *50)
+print("hi " *50) # displays the word hi for 50 times using the '*' symbol which is to multiply
